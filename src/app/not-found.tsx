7516a1a335e5cbbdec1f12ddfb16ec81a3d64bea
René Esteves <rene@robobeau.com>
2024-09-16 19:30:38 -0400

    Fleshing out NotFound template

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import Button from "@/components/Button/Button";
 import ProgramWindow from "@/components/ProgramWindow/ProgramWindow";
+import vt323 from "@/fonts/vt323";
+import { useRouter } from "next/navigation";
 
 const NotFound: React.FC = () => {
+  const router = useRouter();
+
   return (
-    <ProgramWindow className="z-[1000]" origin="m" title="404">
-      <span>Not Found</span>
+    <ProgramWindow
+      className="z-50"
+      minConstraints={[192, 160]}
+      maxConstraints={[192, 160]}
+      origin="m"
+      size={{ height: 160, width: 192 }}
+      title="404"
+    >
+      <div
+        className={`flex flex-col gap-2 h-full justify-center items-center text-xl ${vt323.className}`}
+      >
+        <span>Not Found</span>
+
+        <Button onClick={router.back}>OK</Button>
+      </div>
     </ProgramWindow>
   );
 };
