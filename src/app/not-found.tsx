cac966dfdf0453e66b27f2a301c176ac967e2421
René Esteves <rene@robobeau.com>
2024-09-28 21:40:19 -0400

    Update Not Found template

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,30 +1,31 @@
 "use client";
 
 import Button from "@/components/Button/Button";
-import ProgramWindow from "@/components/ProgramWindow/ProgramWindow";
-import vt323 from "@/fonts/vt323";
+import Program from "@/components/Program/Program";
+import w95fa from "@/fonts/w95fa";
 import { useRouter } from "next/navigation";
 
 const NotFound: React.FC = () => {
   const router = useRouter();
 
   return (
-    <ProgramWindow
-      className="z-50"
+    <Program
+      isMaximizable={false}
+      isMinimizable={false}
       minConstraints={[192, 160]}
       maxConstraints={[192, 160]}
       origin="m"
-      size={{ height: 160, width: 192 }}
+      initialSize={{ height: 160, width: 192 }}
       title="404"
     >
       <div
-        className={`flex flex-col gap-2 h-full justify-center items-center text-xl ${vt323.className}`}
+        className={`flex flex-col gap-2 h-full justify-center items-center text-xl ${w95fa.className}`}
       >
         <span>Not Found</span>
 
         <Button onClick={router.back}>OK</Button>
       </div>
-    </ProgramWindow>
+    </Program>
   );
 };
 
