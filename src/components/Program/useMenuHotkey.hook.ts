22c1bbd2626f056fd96e785f4e18ca24c20ec662
René Esteves <rene@robobeau.com>
2024-09-29 18:25:27 -0400

    useMenuHotkey refactor

--- src/components/Program/useMenuHotkey.hook.ts
+++ src/components/Program/useMenuHotkey.hook.ts
@@ -1,35 +1,42 @@
-import { MENU_HOTKEY_EVENT } from "@/constants";
-import MenuHotkeyEvent from "@/events/MenuHotkey";
-import { useCallback, useEffect } from "react";
+import debounce from "lodash.debounce";
+import { Ref, useCallback, useEffect } from "react";
 import { MenuItem } from "./Menu";
 
-function useMenuHotkey(menu: Array<MenuItem>) {
+function useMenuHotkey(menu: Array<MenuItem>, ref: Ref<HTMLElement>) {
   const menuHotkeyHandler = useCallback(
-    (event: MenuHotkeyEvent) => {
-      const { key } = event.detail;
-      const menuItem = menu.find(
-        ({ key: menuItemKey }) =>
-          menuItemKey.toLowerCase() === key.toLowerCase()
-      );
+    debounce(
+      (event: KeyboardEvent) => {
+        const { altKey, key } = event;
 
-      if (menuItem) {
-        window.open(menuItem.url, menuItem.target);
-      }
-    },
+        if (
+          typeof ref !== "object" ||
+          !ref?.current?.contains(document.activeElement)
+        ) {
+          return false;
+        }
+
+        if (altKey) {
+          const menuItem = menu.find(
+            ({ key: menuItemKey }) =>
+              menuItemKey.toLowerCase() === key.toLowerCase()
+          );
+
+          if (menuItem) {
+            window.open(menuItem.url, menuItem.target);
+          }
+        }
+      },
+      100,
+      { leading: true, trailing: false }
+    ),
     [menu]
   );
 
   useEffect(() => {
-    document.addEventListener(
-      MENU_HOTKEY_EVENT,
-      menuHotkeyHandler as EventListener
-    );
+    document.addEventListener("keydown", menuHotkeyHandler);
 
     return () => {
-      document.removeEventListener(
-        MENU_HOTKEY_EVENT,
-        menuHotkeyHandler as EventListener
-      );
+      document.removeEventListener("keydown", menuHotkeyHandler);
     };
   }, [menuHotkeyHandler]);
 }
--- src/events/MenuHotkey.ts
+++
@@ -1,9 +0,0 @@
-import { MENU_HOTKEY_EVENT } from "@/constants";
-
-class MenuHotkey extends CustomEvent<{ key: string }> {
-  constructor(key: string) {
-    super(MENU_HOTKEY_EVENT, { detail: { key } });
-  }
-}
-
-export { MenuHotkey as default };
