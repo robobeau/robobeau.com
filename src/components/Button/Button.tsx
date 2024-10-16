10451b873bdb20461ade94f69df6d4e2d57ad602
René Esteves <rene@robobeau.com>
2024-10-15 20:03:27 -0400

    Clean up Button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,7 @@
 import { ButtonHTMLAttributes, FC } from "react";
 
+import "@/components/Button/button.css";
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   hasOutline?: boolean;
   hasRoundedCorners?: boolean;
@@ -17,37 +19,20 @@ const Button: FC<ButtonProps> = (props) => {
   } = props;
 
   // #region Classes
-  const outlineClasses = hasOutline
-    ? "focus-within:outline focus-within:outline-1 focus-within:outline-black"
-    : "";
-  const roundedCornerClasses = hasRoundedCorners ? "rounded-sm" : "";
   const buttonClasses = `
-    group/button
-    bg-slate-200 border border-black leading-4
-    ${outlineClasses}
-    ${roundedCornerClasses}
-    ${className}
+    button
+    ${hasOutline ? "button--with-outline" : ""}
+    ${hasRoundedCorners ? "button--with-rounded-corners" : ""}
   `.trim();
-  const textOutlineClasses = hasTextOutline
-    ? `
-      group-active/button:outline group-active/button:outline-1 group-active/button:outline-dotted group-active/button:outline-slate-400
-      group-focus-within/button:outline group-focus-within/button:outline-1 group-focus-within/button:outline-dotted group-focus-within/button:outline-slate-400
-    `.trim()
-    : "";
   const textClasses = `
-    px-1 relative rounded-sm
-    group-active/button:left-0.5 group-active/button:top-0.5
-    ${textOutlineClasses}
-  `.trim();
-  const textWrapperClasses = `
-    border-2 border-b-slate-400 border-l-white border-r-slate-400 border-t-white flex grow h-full items-center justify-center w-full
-    group-active/button:border-b-slate-200 group-active/button:border-l-slate-400 group-active/button:border-r-slate-200 group-active/button:border-t-slate-400 group-active/button:shadow-inner
+    button--text
+    ${hasTextOutline ? "button--text-with-outline" : ""}
   `.trim();
   // #endregion
 
   return (
-    <button className={buttonClasses} {...rest}>
-      <span className={textWrapperClasses}>
+    <button className={`${buttonClasses} ${className}`} {...rest}>
+      <span className="button--text-wrapper">
         <span className={textClasses}>{children}</span>
       </span>
     </button>
