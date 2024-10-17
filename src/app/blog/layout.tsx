e047567b3c01464f927c130216c13f082e159871
René Esteves <rene@robobeau.com>
2024-10-17 07:35:55 -0400

    Remove metadata from Blog layout

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,9 +1,3 @@
-import { Metadata } from "next";
-
 import BlogProgram from "@/components/BlogProgram/BlogProgram";
 
-const metadata: Metadata = {
-  title: "Blog",
-};
-
-export { BlogProgram as default, metadata };
+export { BlogProgram as default };
